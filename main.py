01450e779b27e3fd6ac5a30fc8d275be517cb47b
Mattiavr <167307063+Mattiavr@users.noreply.github.com>
2024-05-11 17:24:52 +0200

    main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from moduli.ChiediDati import *
-from moduli.FunzioniMenu import *
+import moduli.ChiediDati as ChiediDati
+import moduli.FunzioniMenu as FunzioniMenu
 from moduli.Impostazioni import *
-import moduli.FunzioniOrario
+import moduli.FunzioniOrario as FunzioniOrario
 
 
 
 def menu():
-    scelta = FunzioniMenu.Menu(MENU)
+    scelta = FunzioniMenu.menu(MENU)
     return scelta
 
 def main():
@@ -19,27 +19,31 @@ def main():
             uscita = True
         else:
             if (scelta_utente == 1): 
-                orario_istituto = ChiediDati.ChiediStringa("Inserisci il percorso del file dell'orario dei docenti")
-                classe_istituto = ChiediDati.ChiediStringa("Inserisci una delle classi dell'orario")
-                FunzioniOrario.docenti_classe(orario_istituto, classe_istituto)
+                orario_istituto = ChiediDati.chiediStringa("Inserisci il percorso del file dell'orario dei docenti")
+                classe_istituto = ChiediDati.chiediStringa("Inserisci una delle classi dell'orario")
+                file_output = ChiediDati.chiediStringa("Inserisci il percorso del file in cui scrivere l'elaborazione della funzione scelta")
+                FunzioniOrario.docenti_classe(orario_istituto, classe_istituto, file_output)
             
             elif (scelta_utente == 2):
-                orario_istituto = ChiediDati.ChiediStringa("Inserisci il percorso del file dell'orario dei docenti")
-                docente_istituto = ChiediDati.ChiediStringa("Inserisci un docente dell'orario")
-                FunzioniOrario.orario_docente(orario_istituto, docente_istituto)
+                orario_istituto = ChiediDati.chiediStringa("Inserisci il percorso del file dell'orario dei docenti")
+                docente_istituto = ChiediDati.chiediStringa("Inserisci un docente dell'orario")
+                file_output = ChiediDati.chiediStringa("Inserisci il percorso del file in cui scrivere l'elaborazione della funzione scelta")
+                FunzioniOrario.orario_docente(orario_istituto, docente_istituto, file_output)
             
             elif (scelta_utente == 3):
-                orario_istituto = ChiediDati.ChiediStringa("Inserisci il percorso del file dell'orario dei docenti")
-                docente_istituto = ChiediDati.ChiediStringa("Inserisci un docente dell'orario")
-                FunzioniOrario.ore_diposizione(orario_istituto, docente_istituto)
+                orario_istituto = ChiediDati.chiediStringa("Inserisci il percorso del file dell'orario dei docenti")
+                docente_istituto = ChiediDati.chiediStringa("Inserisci un docente dell'orario")
+                file_output = ChiediDati.chiediStringa("Inserisci il percorso del file in cui scrivere l'elaborazione della funzione scelta")
+                FunzioniOrario.ore_diposizione(orario_istituto, docente_istituto, file_output)
                 
             elif (scelta_utente == 4):
-                orario_istituto = ChiediDati.ChiediStringa("Inserisci il percorso del file dell'orario dei docenti")
-                ora = ChiediDati.ChiediStringa("Inserisci un giorno dell'orario")
-                giorno = ChiediDati.ChiediStringa("Inserisci un' ora dell'orario")
-                FunzioniOrario.ore_disponibile(orario_istituto, ora, giorno)
+                orario_istituto = ChiediDati.chiediStringa("Inserisci il percorso del file dell'orario dei docenti")
+                ora = ChiediDati.chiediStringa("Inserisci un'ora dell'orario")
+                giorno = ChiediDati.chiediStringa("Inserisci un giorno dell'orario")
+                file_output = ChiediDati.chiediStringa("Inserisci il percorso del file in cui scrivere l'elaborazione della funzione scelta")
+                FunzioniOrario.ore_disponibile(orario_istituto, ora, giorno, file_output)
            
             scelta_utente = menu()
     
     
-main()           
+main()   
